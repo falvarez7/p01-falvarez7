925f4f6e5889df69371d44dd2cced434f822ecc9
falvarez7 <falvarez7@mail.csuchico.edu>
2017-09-25 23:05:43 -0700

    This is the final version

--- guess.cpp
+++ guess.cpp
@@ -1,4 +1,4 @@
-#include <cstdlib>
+#include<cstdlib>
 #include<iostream>
 #include<string>
 #include<ctime>
@@ -10,21 +10,22 @@ int guess;
 srand (time(NULL) );
 int randomNumber;
 randomNumber = rand()%10+1;
+
+
 cout<<"Let's play a game."<<endl<<"Guess a number from 1 to 10."<<endl;
-//cout<<randomNumber<<endl;
+/*cout<<randomNumber<<endl;*///Allows for testing of the game.
 cin>>guess;
-if(randomNumber==guess){
-cout<<"Holy Moly! Your guess of "<<guess<<" was correct!"<<endl;}
-else if(randomNumber<guess){
-int less;
-less = guess - randomNumber;
-cout<<"Dang! You were close. You missed by "<<less<<endl;}
-else
-{ int more;
-more = randomNumber-guess;
-cout<<"Dang! You were close. You missed by "<<more<<endl;}
+    if(randomNumber==guess){
+        cout<<"You guessed correctly! You win!"<<endl;}
+    else if(randomNumber<guess){
+        int less;
+        less = guess - randomNumber;
+        cout<<"You were off by "<<less<<". Sorry, you lose. "<<endl;}
 
-
+    else{
+        int more;
+        more = randomNumber-guess;
+        cout<<"You were off by "<<more<<". Sorry, you lose. "<<endl;}
 
 return 0;}
 
